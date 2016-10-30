0a2ac32ba5dfdb8f125b9551bb07f9cae4d9c1fb
alexey-ernest <alexey.ernest@gmail.com>
2016-10-30 14:19:32 +0300

    Node TensorFlow Serving gRPC client

--- serving/client/index.js
+++ serving/client/index.js
@@ -1,11 +1,11 @@
 
-var PROTO_PATH = __dirname + '/protos/prediction_service.proto';
+const PROTO_PATH = __dirname + '/protos/prediction_service.proto';
 
-var grpc = require('grpc');
-var tensorflow_serving = grpc.load(PROTO_PATH).tensorflow.serving;
+const grpc = require('grpc');
 
 module.exports = (connection) => {
   
+  var tensorflow_serving = grpc.load(PROTO_PATH).tensorflow.serving;
   var client = new tensorflow_serving.PredictionService(
     connection, grpc.credentials.createInsecure()
   );
@@ -15,21 +15,47 @@ module.exports = (connection) => {
     /**
      * Calls predict gRPC method on TensorFlow Serving server.
      *
-     * @param      {Function}  fn      Callback.
+     * @param      {ReadStream}  stream  JPEG read stream for classification.
+     * @param      {Function}    fn      Callback.
      */
-    predict: (fn) => {
-      var msg = {
-        model_spec: { name: 'inception' },
-        inputs: {
-          images: null
-        }
-      };
+    predict: (stream, fn) => {
 
-      client.predict(msg, (err, response) => {
-        if (err) return fn(err);
-
-        fn(null, response.message)
+      // reading stream
+      var buffers = [];
+      stream.on('data', (buffer) => {
+        buffers.push(buffer);
       });
+      stream.on('end', () => {
+        var buffer = Buffer.concat(buffers);
+
+        // building PredictRequest proto message
+        const msg = {
+          model_spec: { name: 'inception' },
+          inputs: {
+            images: {
+              dtype: "DT_STRING",
+              tensor_shape: {
+                dim: {
+                  size: 1
+                }
+              },
+              string_val: buffer
+            }
+          }
+        };
+
+        // calling gRPC method
+        client.predict(msg, (err, response) => {
+          if (err) return fn(err);
+
+          // decoding response
+          const classesBufs = response.outputs.classes.string_val;
+          const classes = classesBufs.map((b) => b.toString('utf8'));
+
+          fn(null, classes)
+        });
+      });
+      
     }
   }
 };
--- serving/client/test.js
+++ serving/client/test.js
@@ -1,6 +1,14 @@
 
-var client = require('./')('192.168.99.100:9000');
+const fs = require('fs');
+const predictionClient = require('./')('192.168.99.100:9000');
 
-client.predict((err, res) => {
+const IMAGE_PATH = './cat.jpg';
+const imageStream = fs.createReadStream(IMAGE_PATH);
+
+predictionClient.predict(imageStream, (err, res) => {
+  if (err) {
+    return console.error(err);
+  }
+
   console.log('Response:', res);
 });
