35e42614dcd3cf913b3dcd180e2ba1ec7fb7168a
alexey-ernest <alexey.ernest@gmail.com>
2016-10-30 16:09:45 +0300

    API using Node client

--- api/index.js
+++ api/index.js
@@ -1,26 +1,16 @@
-var http = require('http');
-var fs = require('fs');
-var tmp = require('tmp');
-var debug = require('debug')('scrt');
-var exec = require('child_process').exec;
-var Busboy = require('busboy');
 
-var SCRIPT_COMMAND = process.env.SCRIPT_COMMAND || '';
+const http = require('http');
+const Busboy = require('busboy');
+const debug = require('debug')('scrt');
+const predictionClient = require('../serving/client')('192.168.99.100:9000');
 
-function execScript(cmd, cb) {
-  exec(cmd, function (err, stdout, stderr) {
-    if (err) return cb(err);
-    cb(null, stdout);
-  });
-}
-
-function handleError(err, res) {
+const handleError = (err, res) => {
   res.statusCode = 500;
   res.end('Internal Server Error:' + err);
 }
 
-var server = http.createServer(function (req, res) {
-  // todo: check if the method is POST and type is multipart-data
+const server = http.createServer((req, res) => {
+
   if (req.method !== 'POST') {
     res.statusCode = 404;
     return res.end();
@@ -28,44 +18,40 @@ var server = http.createServer(function (req, res) {
 
   var files = [];
 
-  var busboy = new Busboy({ headers: req.headers });
-  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+  var busboy = new Busboy({ headers: req.headers });  
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    
     if (mimetype !== 'image/jpeg') {
       res.statusCode = 415;
-      file.resume();
-      return;
+      return file.resume();
     }
 
-    // create temp file
-    tmp.file(function(err, path) {
-      if (err) {
-        handleError(err, res);
-        file.resume();
-      }
-
-      files.push(path);
-
-      // upload file
-      debug('Uploading file ' + filename + ' to ' + path);
-      file.pipe(fs.createWriteStream(path));
+    var buffers = [];
+    file.on('data', (data) => {
+      buffers.push(data);
     });
+    file.on('end', () => {
+      files.push(Buffer.concat(buffers));
+    });
+
   });
-  busboy.on('finish', function() {
+  busboy.on('finish', () => {
+    
     if (files.length === 0) {
       res.statusCode = 400;
-      return res.end('0 JPEGs were uploaded.');
+      return res.end('No JPEGs were uploaded.');
     }
 
-    var path = files[0];
-    var cmd = SCRIPT_COMMAND.replace('$image', path);
-    debug('Executing script: ' + cmd);
-    execScript(cmd, function (err, stdout) {
+    predictionClient.predict(files, (err, results) => {
       if (err) return handleError(err, res);
 
-      debug('Result of script execution: ' + stdout);
-      res.end(stdout);
-    });
+      debug('Results of image inference:', results);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(results));
+    });    
   });
+
   return req.pipe(busboy);
 });
 
